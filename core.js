c3567dc0254665334993ad920700fce07c638b7e
Никулин Антон Леонидович <nikulin@ek.apress.ru>
2016-11-15 16:38:09 +0500

    Начал переносить приложение на Electron

--- core.js
+++ core.js
@@ -1,5 +1,40 @@
-'use strict';
+const {
+    app, BrowserWindow
+} = require('electron')
+const path = require('path')
+const url = require('url')
 
-const server = require('server');
+let win
 
-server.run();
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '/view/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
